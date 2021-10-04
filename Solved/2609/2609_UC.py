21cee1326521940f4df39b8e9d2b42a09cea44a4
alsrua7222 <59680587+alsrua7222@users.noreply.github.com>
2021-10-05 02:00:45 +0900

    Create 2609_UC.py

---
+++ Solved/2609/2609_UC.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+
+
+def gcd(n, m):
+    while m:
+        n, m = m, n % m
+    return n
+
+
+def lcm(n, m):
+    return (n * m) // gcd(n, m)
+
+
+print(gcd(N, M))
+print(lcm(N, M))

39ab066ac6702fccfda3a9eb90ba862cc3d2c658
alsrua7222 <59680587+alsrua7222@users.noreply.github.com>
2022-02-27 00:39:01 +0900

    파싱 에러 해결
    세션으로 요청 접근 방식으로 수정

--- makeBOJFolder/makeBOJFolder.py
+++ makeBOJFolder/makeBOJFolder.py
@@ -1,5 +1,10 @@
 import os, sys, re, requests
 
+# 양식
+# python makeBOJFolder.py (문제번호) (복사할 파일경로) (블로그 주소)
+# python makeBOJFolder.py 1269 Ebay/1.py https://blog.naver.com/alsrua7222/222658769362
+# python makeBOJFolder.py 2580 C:\Users\KMK\source\repos\CNote\CNote\소스.cpp https://blog.naver.com/alsrua7222/222634634258
+
 def makeDirectory(number):
     if not os.path.exists(f"{number}"):
         os.makedirs(f"{number}")
@@ -28,7 +33,9 @@ def makeSourceFile(number, path, blog_path):
 
 def makeReadMeFile(number, blog_path):
     url = f"https://www.acmicpc.net/problem/{number}"
-    title = re.findall("<title>[0-9]+번: (.*?)</title>", requests.get(url).text)[0]
+    s = requests.session()
+    s.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'})
+    title = re.findall("""<span id="problem_title">(.*?)</span>""", s.get(url).text)[0]
     README_CONTEXT = f"# {title}\n" \
                      f"{url}\n" \
                      f"## 해결 과정\n" \

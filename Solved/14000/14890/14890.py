1cd01e1d6f54e7c9cc91922fa502f0de7448ca7c
alsrua7222 <alsrua0531@gmail.com>
2022-06-20 01:23:09 +0900

    구현

--- Solved/14000/14890/14890.py
+++ Solved/14000/14890/14890.py
@@ -1,43 +1,43 @@
 N, L = map(int, input().split())
 MAP = [list(map(int, input().split())) for _ in range(N)]
-answer = 0
 
-rowMAP = []
-for x in range(N):
-    tmp = []
-    cnt = 1
-    pre = MAP[x][0]
-    for y in range(1, N):
-        if pre == MAP[x][y]:
-            cnt += 1
-        else:
-            tmp.append((pre, cnt))
-            pre = MAP[x][y]
-            cnt = 1
-    tmp.append((pre, cnt))
-    rowMAP.append(tmp)
-
-colMAP = []
-for y in range(N):
-    tmp = []
-    cnt = 1
-    pre = MAP[0][y]
-    for x in range(1, N):
-        if pre == MAP[x][y]:
-            cnt += 1
-        else:
-            tmp.append((pre, cnt))
-            pre = MAP[x][y]
-            cnt = 1
-    tmp.append((pre, cnt))
-    colMAP.append(tmp)
-
-def Check(arr):
-    success = True
-    diff = arr[i][0] - arr[i + 1][0]
-    for i in range(len(arr) - 1):
-        diff = abs(arr[i][0] - arr[i+1][0])
-        if diff >= 2:
-            return False
-        if arr[i + 1][1] < L 
+def IsAble(arr):
+    visited = [False] * (N + 1)
+    for i in range(1, N):
+        if arr[i - 1] != arr[i]:
+            diff = abs(arr[i] - arr[i - 1])
+            if diff != 1:
+                return False
+                
+            if arr[i - 1] < arr[i]:
+                for j in range(1, L + 1):
+                    if i - j < 0:
+                        return False
+                    if arr[i - 1] != arr[i - j]:
+                        return False
+                    if visited[i - j]:
+                        return False
+                    visited[i - j] = True
+            else:
+                for j in range(L):
+                    if i + j >= N:
+                        return False
+                    if arr[i] != arr[i + j]:
+                        return False
+                    if visited[i + j]:
+                        return False
+                    visited[i + j] = True
     return True
+answer = 0
+for x in range(N):
+    if IsAble(MAP[x]):
+        answer += 1
+
+for y in range(N):
+    col = []
+    for x in range(N):
+        col.append(MAP[x][y])
+    if IsAble(col):
+        answer += 1
+
+print(answer)

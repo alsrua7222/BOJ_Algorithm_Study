68ca71031299e4990ce93fc62242baacb5405d27
alsrua7222 <alsrua0531@gmail.com>
2022-06-29 02:55:58 +0900

    BFS

---
+++ Solved/24000/24445/24445.py
@@ -0,0 +1,26 @@
+from collections import defaultdict, deque
+
+N, M, R = map(int, input().split())
+Graph = defaultdict(list)
+for _ in range(M):
+    u, v = map(int, input().split())
+    Graph[u].append(v)
+
+for key in Graph.keys():
+    Graph[key].sort(reverse=True)
+def BFS(start):
+    Queue = deque([start])
+    count = [0] * (N + 1)
+    count[start] = 1
+    cnt = 1
+    while Queue:
+        start = Queue.popleft()
+        for s in Graph[start]:
+            if count[s] != 0:
+                continue
+            Queue.append(s)
+            cnt += 1
+            count[s] = cnt
+    return count[1:]
+
+print(*BFS(R), sep="\n")

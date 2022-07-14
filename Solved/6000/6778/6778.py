5b55e8afc715805bc64952c66a2bb403e3af278f
alsrua7222 <alsrua0531@gmail.com>
2022-07-15 01:25:57 +0900

    구현

---
+++ Solved/6000/6778/6778.py
@@ -0,0 +1,9 @@
+a = int(input())
+m = int(input())
+
+if 3 <= a and m <= 4:
+    print("TroyMartian")
+if a <= 6 and 2 <= m:
+    print("VladSaturnian")
+if a <= 2 and m <= 3:
+    print("GraemeMercurian")
